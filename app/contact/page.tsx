494d7f6d5170e3615c1f334d18c28cca95f8bcbc
Declan <declanobrien77@gmail.com>
2025-12-03 00:04:02 +0000

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "../components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center p-6">
+      <ContactForm />
+    </main>
+  );
+}
+
