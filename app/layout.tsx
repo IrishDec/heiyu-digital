0a9c006edda946665731d110f57ae512cec1926e
Declan <declanobrien77@gmail.com>
2025-12-20 22:13:50 +0000

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Heiyu Digital",
   description: "High-performance web solutions.",
-  icons: {
-    icon: "/images/favicon.png",
-  },
+icons: {
+  icon: "/favicon.ico",
+},
 };
 
 export default function RootLayout({
