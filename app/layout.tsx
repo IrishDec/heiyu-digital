6af30311a9c8b4a5891e3a367594b64879dd927a
Declan <declanobrien77@gmail.com>
2025-12-20 23:07:57 +0000

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Heiyu Digital",
-  description: "High-performance web solutions.",
+  title: "Heiyu Digital | Dublin Web Apps",
+  description: "Dublin-based high-performance web apps and systems.",
   verification: {
     google: "NRtudc68IJ9OR9WiLTIgUYK6gh3ACq6SOYikxjIOIoY",
   },
